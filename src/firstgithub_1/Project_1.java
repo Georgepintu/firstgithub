bb0f3f1f14a5dc676ac4c35faaefee29320a66f1
Georgepintu <102867086+Georgepintu@users.noreply.github.com>
2022-04-18 21:27:31 -0400

    2nd_time

--- src/firstgithub_1/Project_1.java
+++ src/firstgithub_1/Project_1.java
@@ -5,7 +5,7 @@ public class Project_1 {
 	public static void main(String[] args) {
 		//First online sharing Project (github)
 
-		System.out.println("Hi mera dil!!");
+		System.out.println("Happy Easter 2022");
 	}
 
 }
