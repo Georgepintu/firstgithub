fc3da14109df3fa9498ebd17b49012550b7f8d29
Georgepintu <102867086+Georgepintu@users.noreply.github.com>
2022-04-18 21:18:29 -0400

    first_sharing

---
+++ src/firstgithub_1/Project_1.java
@@ -0,0 +1,11 @@
+package firstgithub_1;
+
+public class Project_1 {
+
+	public static void main(String[] args) {
+		//First online sharing Project (github)
+
+		System.out.println("Hi mera dil!!");
+	}
+
+}
